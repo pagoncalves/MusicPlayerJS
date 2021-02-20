24d899b874fb47cbdfeae4a08b8dcde84eeae8c1
Patricia Goncalves <patriciagpereira89@gmail.com>
2021-02-20 10:08:57 -0300

    Criado Display

--- mplayer.js
+++ mplayer.js
@@ -2,61 +2,71 @@ var tocando = false;
 var ligado = false;
 var faixa = Number(1);
 var volume = Number(0);
-var btlig = window.document.getElementById("ligar")
+var btlig = window.document.getElementById("ligar");
+var dtoca = window.document.getElementById("distoc");
+var dvol = window.document.getElementById("disvol");
+var dfx = window.document.getElementById("disfai");
 
-function on(){
- if (ligado == true){
-     tocando = false;
-    volume = 0;
-    ligado = false;
-    btlig.style.backgroundColor = 'red';
-    btlig.innerText = "Desligado";
- } else {
-    btlig.style.backgroundColor = 'green';
-    btlig.innerText = "Ligado";
-    ligado = true;
-
- }
-}
-function Play(){
-    if (ligado == true){
-        tocando = true;
-    }
-}
-
-function Pause(){
-    if(ligado == true){
+function on() {
+    if (ligado == true) {
         tocando = false;
+        volume = 0;
+        ligado = false;
+        btlig.style.backgroundColor = 'red';
+        btlig.innerText = "Desligado";
+    } else {
+        btlig.style.backgroundColor = 'green';
+        btlig.innerText = "Ligado";
+        ligado = true;
+
+    }
+}
+function Play() {
+    if (ligado == true) {
+        tocando = true;
+        dtoca.innerText = "Tocando"
     }
 }
 
-function Parar(){
-    if(ligado == true){
+function Pause() {
+    if (ligado == true) {
+        tocando = false;
+        dtoca.innerText = "Pausado"
+    }
+}
+
+function Parar() {
+    if (ligado == true) {
         tocando = false;
         faixa = 1;
+        dtoca.innerText = "Parado";
     }
 }
 
-function Mais(){
-    if (ligado == true && tocando == true && volume <=20 ){
+function Mais() {
+    if (ligado == true && tocando == true && volume <20) {
         volume = volume + 2;
+        dvol.innerHTML = volume;
     }
 }
 
-function Menos(){
-    if (ligado == true && volume >=0){
+function Menos() {
+    if (ligado == true && tocando && volume > 0) {
         volume = volume - 2;
+        dvol.innerHTML = volume;
     }
 }
 
-function Prox(){
-    if(ligado == true && faixa <=10){
+function Prox() {
+    if (ligado == true && faixa < 10) {
         faixa = faixa + 1;
+        dfx.innerHTML = faixa;
     }
 }
 
-function Ant(){
-    if(ligado == true && faixa >=0){
+function Ant() {
+    if (ligado == true && faixa > 0) {
         faixa = faixa - 1;
+        dfx.innerHTML = faixa;
     }
 }
